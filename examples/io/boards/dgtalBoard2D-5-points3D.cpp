266877f5da2687fd5eab444ba10a5b2efb387975
Xavier Provençal <xavier.provencal@univ-smb.fr>
2016-09-21 13:50:53 +0200

    Add projection mode to draw 3D points into 2D board

---
+++ examples/io/boards/dgtalBoard2D-5-points3D.cpp
@@ -0,0 +1,71 @@
+
+/**
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+/**
+ * @file io/boards/dgtalBoard2D-1-points.cpp
+ * @ingroup Examples
+ * @author Jacques-Olivier Lachaud (\c jacques-olivier.lachaud@univ-savoie.fr )
+ * Laboratory of Mathematics (CNRS, UMR 5807), University of Savoie, France
+ *
+ * @date 2010/11/26
+ *
+ * An example file named dgtalBoard2D-1-points.
+ * 
+ */
+
+///////////////////////////////////////////////////////////////////////////////
+#include <iostream>
+#include "DGtal/base/Common.h"
+#include "DGtal/io/boards/Board2D.h"
+#include "DGtal/helpers/StdDefs.h"
+///////////////////////////////////////////////////////////////////////////////
+
+using namespace std;
+using namespace DGtal;
+using namespace DGtal::Z3i;
+
+///////////////////////////////////////////////////////////////////////////////
+
+int main()
+{
+  trace.beginBlock ( "Example dgtalBoard2D-5-points3D" );
+
+  Point p1( -2, -2, 2 );
+  Point p2( 1, -1, 0 );
+  Point p3( 2, 2, 2 );
+  Domain domain( p1, p3 );
+  
+  Board2D board;
+  board << SetMode( p1.className(), "Projection1" );
+  board << domain << p1 << p2 << p3;
+
+  //board.saveSVG("dgtalBoard2D-1-points.svg");
+  //board.saveEPS("dgtalBoard2D-1-points.eps");
+  //board.saveTikZ("dgtalBoard2D-1-points.tikz");
+
+#ifdef WITH_CAIRO
+  //board.saveCairo("dgtalBoard2D-1-points-cairo.pdf", Board2D::CairoPDF);
+  board.saveCairo("dgtalBoard2D-1-points-cairo.png", Board2D::CairoPNG);
+  //board.saveCairo("dgtalBoard2D-1-points-cairo.ps", Board2D::CairoPS);
+  //board.saveCairo("dgtalBoard2D-1-points-cairo.svg", Board2D::CairoSVG);
+#endif
+  
+  trace.endBlock();
+  return 0;
+}
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
