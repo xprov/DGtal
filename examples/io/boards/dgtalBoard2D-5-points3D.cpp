0e9f0536171deddf0d427708fe6a30df3a203da1
Xavier Provençal <xavier.provencal@univ-smb.fr>
2016-09-22 18:26:53 +0200

    Object with adjacency projects ok, working on gridcurves

--- examples/io/boards/dgtalBoard2D-5-points3D.cpp
+++ examples/io/boards/dgtalBoard2D-5-points3D.cpp
@@ -33,41 +33,91 @@
 #include "DGtal/io/boards/Board2D.h"
 #include "DGtal/io/Color.h"
 #include "DGtal/helpers/StdDefs.h"
+#include "DGtal/shapes/Shapes.h"
 ///////////////////////////////////////////////////////////////////////////////
 
 using namespace std;
 using namespace DGtal;
-using namespace DGtal::Z3i;
 
 ///////////////////////////////////////////////////////////////////////////////
+//
+const Color red   ( 255,   0,   0 );
+const Color dred  ( 192,   0,   0 );
+const Color green ( 0,   255,   0 );
+const Color dgreen( 0,   192,   0 );
+const Color blue  ( 0,     0, 255 );
+const Color dblue ( 0,     0, 192 );
+
+struct DigitalPlane
+{
+  typedef Z3i::Point Point;
+  typedef Z3i::Vector Vector;
+  typedef Z3i::Point::Component Integer;
+  DigitalPlane( Vector v, Integer mu, Integer omega ) : 
+    v(v), mu(mu), omega(omega)
+  {}
+  bool operator()( const Point& p ) const
+    {
+      int height = p.dot(v);
+      return mu <= height && height < mu + omega;
+    }
+  Vector v;
+  Integer mu;
+  Integer omega;
+};
 
 int main()
 {
   trace.beginBlock ( "Example dgtalBoard2D-5-points3D" );
 
-  Point p1( -2, -2, -2 );
-  Point p2( 1, 0, 0 );
-  Point p3( 0, 1, 0 );
-  Point p4( 0, 0, 1 );
-  Point p5( 1, 1, 0 );
-  Point p6( 1, 0, 1 );
-  Point p7( 0, 1, 1 );
-  //Point p8( 1, 1, 1 );
-  //Point p9( 2, 2, 2 );
-  Z2i::Domain domain( Z2i::Point(-3,-3), Z2i::Point(3,3) );
-  //Domain domain( p1, p3 );
-  
-  Board2D board( Color::White );
-  board << SetMode( p1.className(), "Grid" );
-  board << domain << p2 << p3 << p4 << p5 << p6 << p7;
-  board << SetProjection( {-3.0,-2.0, 2.0, -2.0, 0.0, 3.0} );
-  board << p2 << p3 << p4 << p5 << p6 << p7;
+  Z2i::Domain domain2D( Z2i::Point(-5,-5),    Z2i::Point(5,5) );
+  Z3i::Domain domain3D( Z3i::Point(-5,-5,-5), Z3i::Point(5,5,5) );
 
-#ifdef WITH_CAIRO
-  board.saveCairo("dgtalBoard2D-5-points3D.png", Board2D::CairoPNG);
-#endif
+
+  DigitalSetBySTLVector<Z3i::Domain> S( domain3D );
+  std::vector<Z3i::Point> v( { Z3i::Point( 1,0,0 ),
+                               Z3i::Point( 0,1,0 ),
+                               Z3i::Point( 0,0,1 ),
+                               Z3i::Point( 1,1,0 ),
+                               Z3i::Point( 1,0,1 ),
+                               Z3i::Point( 0,1,1 ) } );
+  S.insert( v.begin(), v.end() );
+
+  Board2D board( Color::White );
+  board << SetMode( S.className(), "Grid" );
+  //board << domain2D;
+  //board << S;
+
+  // Change projection on the fly
+  // board << SetProjection( {-2.0,-2.0, 2.0, -2.0, 0.0, 3.0} );
+  // board << CustomStyle( S.begin()->className(), new CustomColors( red, dred ) );
+  // board << S;
+
+  // Change projection on the fly
+  // typedef LibBoard::Point PrjPoint;
+  // board << CustomStyle( S.begin()->className(), new CustomColors( green, dred ) );
+  // board << SetProjection( { PrjPoint(-4.0,-4.0), PrjPoint(4.0, -4.0), PrjPoint(0.0, 6.0) } );
+  // board << S;
+
+  Z3i::DigitalSet shape_set( domain3D );
+  //Shapes<Z3i::Domain>::addNorm1Ball( shape_set, Z3i::Point( 5, 5, 5 ), 2 );
+  //Shapes<Z3i::Domain>::addNorm2Ball( shape_set, Z3i::Point( 3, 3, 3 ), 2 );
+  DigitalPlane p( Z3i::Vector( 3,5,8 ), 0, 16 );
+  Shapes<Z3i::Domain>::makeSetFromPointPredicate( shape_set, p );
+
+  Z3i::Object6_18 shape( Z3i::dt6_18, shape_set );
+  board << SetMode( shape.className(), "DrawAdjacenciesNoArrow" );
+  // set projection back to default for dimension 3.
+  // board << SetProjection( 3, "default" );
+  board << shape;
+
+  board.saveCairo("out.png", Board2D::CairoPNG);
   
   trace.endBlock();
+
+
+
+
   return 0;
 }
 //                                                                           //
